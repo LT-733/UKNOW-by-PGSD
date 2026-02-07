66732937156a5418da7e601cba8ded4c661ab7b6
Brovvvn <edwardsze3815@gmail.com>
2026-02-07 18:19:38 -0500

    FrontEnd without graph complete, design needs rework

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -20,5 +20,7 @@ from frontendapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('result/', views.result, name='result'),
+    path('detail/', views.detail, name='detail')
 ]
--- frontend/frontendapp/views.py
+++ frontend/frontendapp/views.py
@@ -4,9 +4,22 @@ from django.shortcuts import render
 def home(request):
     context = {
         "app_name": "UKNOW",
-        "tagline": "A basic Django app",
+        "tagline": "A university selection lookup table",
         "features": [
             "testing boi"
         ],
     }
     return render(request, "frontendapp/home.html", context)
+
+
+def result(request):
+    name = request.GET.get("name")
+    gpa = request.GET.get("gpa")
+    uni = request.GET.get("uni")
+
+    return render(request, "frontendapp/result.html")
+
+def detail(request):
+    id = request.GET.get("id")
+
+    return render(request, "frontendapp/detail.html")
