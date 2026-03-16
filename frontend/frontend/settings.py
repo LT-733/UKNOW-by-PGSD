6d84a7d76f0fd0651c3e34065da3118affcd89d6
Kevin Xu <187478443+kodxity@users.noreply.github.com>
2026-03-15 22:04:48 -0400

    vercel db

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -83,16 +83,24 @@ WSGI_APPLICATION = 'frontend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'riku.shoshin.uwaterloo.ca'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+if os.getenv('VERCEL'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('PASSWORD'),
+            'HOST': os.getenv('DB_HOST', 'riku.shoshin.uwaterloo.ca'),
+            'PORT': os.getenv('DB_PORT', '3306'),
+        }
+    }
 
 
 # Password validation
